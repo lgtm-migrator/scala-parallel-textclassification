bb5fee8cfc746faaecb4889e3e81df07f4e3cba9
Van-Duyet Le <me@duyetdev.com>
2015-11-02 13:28:40 +0700

    Update tools generate vietnamese stopword event import file, support load stopwords from github repo of duyetdev

--- tools/create-stop-words.js
+++ tools/create-stop-words.js
@@ -1,102 +1,53 @@
 'use strict';
 
 var fs = require('fs');
+var wget = require('wget');
+
 var path = '../data/stopwords-vietnamese.json';
+var src = 'https://raw.githubusercontent.com/duyetdev/vietnamese-stopwords/master/vietname-stopwords.txt';
+// var output = '/tmp/data.json';
 
-var words = [
-	"bị",
-	"bởi",
-	"cả",
-	"các",
-	"cái",
-	"cần",
-	"càng",
-	"chỉ",
-	"chiếc",
-	"cho",
-	"chứ",
-	"chưa",
-	"chuyện",
-	"có",
-	"có thể",
-	"cứ",
-	"của",
-	"cùng",
-	"cũng",
-	"đã",
-	"đang",
-	"đây",
-	"để",
-	"đến nỗi",
-	"đều",
-	"điều",
-	"do",
-	"đó",
-	"được",
-	"dưới",
-	"gì",
-	"khi",
-	"không",
-	"là",
-	"lại",
-	"lên",
-	"lúc",
-	"mà",
-	"mỗi",
-	"một cách",
-	"này",
-	"nên",
-	"nếu",
-	"ngay",
-	"nhiều",
-	"như",
-	"nhưng",
-	"những",
-	"nơi",
-	"nữa",
-	"phải",
-	"qua",
-	"ra",
-	"rằng",
-	"rằng",
-	"rất",
-	"rất",
-	"rồi",
-	"sau",
-	"sẽ",
-	"so",
-	"sự",
-	"tại",
-	"theo",
-	"thì",
-	"trên",
-	"trước",
-	"từ",
-	"từng",
-	"và",
-	"vẫn",
-	"vào",
-	"vậy",
-	"vì",
-	"việc",
-	"với",
-	"vừa"
-];
+var wget = require('wget');
+var options = {
+    protocol: 'https',
+    host: 'raw.githubusercontent.com',
+    path: '/duyetdev/vietnamese-stopwords/master/vietname-stopwords.txt',
+    // proxy: 'http://host:port',
+    method: 'GET'
+};
+var req = wget.request(options, function(res) {
+    var content = '';
+    if (res.statusCode === 200) {
+        res.on('error', function(err) {
+            console.log(err);
+        });
+        res.on('data', function(chunk) {
+            content += chunk;
+        });
+        res.on('end', function() {
+            fs.unlink(path, function() {
+            	var words = content.split('\n');
+				for (var i in words) {
+					var stopWordRecord = {
+						event: "stopwords",
+						eventTime: new Date(),
+						entityId: i,
+						entityType: "resource",
+						properties: {
+							word: words[i]
+						}
+					}
 
-fs.unlink(path, function() {
-	for (var i in words) {
-		var stopWordRecord = {
-			event: "stopwords",
-			eventTime: new Date(),
-			entityId: i,
-			entityType: "resource",
-			properties: {
-				word: words[i]
-			}
-		}
+					fs.appendFile(path, JSON.stringify(stopWordRecord) + "\n");
+				}
+			});
+        });
+    } else {
+        console.log('Server respond ' + res.statusCode);
+    }
+});
 
-		fs.appendFile(path, JSON.stringify(stopWordRecord) + "\n");
-	}
-
-	console.log("Finish!\nUsing: pio import --appid 1 --input " + path)
+req.end();
+req.on('error', function(err) {
+    console.log(err);
 });
